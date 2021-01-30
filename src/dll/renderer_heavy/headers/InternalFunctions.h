adb0822b851dde7a934ad2ca61ba3a9c1dfc3518
jqwr55 <62599159+jqwr55@users.noreply.github.com>
2021-01-30 17:48:56 +0100

    msg

--- src/dll/renderer_heavy/headers/InternalFunctions.h
+++
@@ -1,8 +0,0 @@
-#include <Types.h>
-
-void ModKeyCallBack(GLFWwindow* window , int key, int scancode, int action, int mods);
-void TextCallBack(GLFWwindow* window , uint codePoint);
-void MouseCallBack(GLFWwindow* w, double x ,double y);
-void WindowCloseCallback(GLFWwindow* window);
-void WindowResizeCallBack(GLFWwindow* window , int w , int h);
-void RenderLoop(Renderer* r);
